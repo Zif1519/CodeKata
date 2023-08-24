d9f9a7987b196187a09950b59bc343962950acd6
Sooyong Choi <forhamgame@gmail.com>
2023-08-24 10:33:33 +0900

    Q1

---
+++ Q1/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Q1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine(solution.solution(3));
+            Console.WriteLine(solution.solution(4));
+        }
+    }
+
+    public class Solution
+    {
+        public string solution(int num)
+        {
+            string answer = "";
+            answer = (num % 2 == 0) ? "Even" : "Odd";
+            return answer;
+        }
+    }
+}
