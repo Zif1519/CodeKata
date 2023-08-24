27f096d228615b06ccbc5618cb91ffded3f14c81
Sooyong Choi <forhamgame@gmail.com>
2023-08-24 10:53:11 +0900

    Q3

--- Q1/Program.cs
+++ Q1/Program.cs
@@ -34,5 +34,20 @@
         }
     }
 
+    public class Solution3
+    {
+        public int solution(int n)
+        {
+            int answer = 0;
+            while (n != 0)
+            {
+                answer += n % 10;
+                n = n / 10;
+            }
+            return answer;
+        }
+    }
+
+
 
 }
