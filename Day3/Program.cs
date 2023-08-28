30eeceba1940b7558db4a9f49e4c845ca81b0451
Sooyong Choi <forhamgame@gmail.com>
2023-08-28 15:10:07 +0900

    [ADD] Day3 and test Connect to Issue #1

---
+++ Day3/Program.cs
@@ -0,0 +1,47 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Day3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution soulution = new Solution();
+            var result = soulution.solution(273811);
+            Console.WriteLine(result);
+        }
+    }
+
+    public class Solution1
+    {
+        public int solution(string s)
+        {
+            int answer = 0;
+            answer = int.Parse(s);
+            return answer;
+        }
+    }
+
+    public class Solution2
+    {
+        public long solution(long n)
+        {
+            long sqrt = (long)Math.Sqrt(n);
+            if (sqrt * sqrt == n) return (sqrt + 1) * (sqrt + 1);
+            else return -1;
+        }
+    }
+    public class Solution
+    {
+        public long solution(long n)
+        {
+            long answer = 0;
+            char[] x = n.ToString().ToCharArray();
+            Array.Sort(x);
+            Array.Reverse(x);
+            answer = long.Parse(new string(x));
+
+            return answer;
+        }
+    }
+}
