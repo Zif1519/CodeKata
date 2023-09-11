86370244519352b07d68bce73db39b8c7aa66414
Zif1519 <141081153+Zif1519@users.noreply.github.com>
2023-09-11 10:08:56 +0900

    23.09.11 코드카타 2문제 완료
    23.09.11 코드카타 2문제 완료
    
    최소사각형, 시저암호

---
+++ Day7/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace Day7
+{
+    using System;
+    using System.Linq;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int[,] insert = new int[,] { { 14, 4 }, { 19, 6 }, { 6, 16 }, { 18, 7 }, { 7, 11 } };
+            string insert = "a B c";
+            Solution soulution = new Solution();
+            var result = soulution.solution(insert,4);
+            Console.WriteLine(result);
+        }
+    }
+
+
+    public class Solution2
+    {
+        public int solution2(int[,] sizes)
+        {
+            int answer = 0;
+            int maxW = 0;
+            int maxH = 0;
+            for (int i = 0; i < sizes.Length; i++)
+            {
+                if (sizes[i, 0] > sizes[i, 1])
+                {
+                    if (maxW < sizes[i, 0]) maxW = sizes[i, 0];
+                    if (maxH < sizes[i, 1]) maxH = sizes[i, 1];
+                }
+                else
+                {
+                    if (maxH < sizes[i, 0]) maxH = sizes[i, 0];
+                    if (maxW < sizes[i, 1]) maxW = sizes[i, 1];
+                }
+            }
+
+            answer = maxW * maxH;
+            return answer;
+        }
+    }
+    public class Solution
+    {
+        public string solution(string s, int n)
+        {
+            string answer = "";
+            char[] chars = s.ToCharArray();
+            for (int i = 0; i < chars.Length; i++)
+            {
+                if (chars[i] <= 'z'&& chars[i] >= 'a')
+                {
+                    chars[i] = (char)((chars[i] + n));
+                    if (chars[i] > 'z') chars[i] = (char)(chars[i] - 26);
+                }
+                if (chars[i] <= 'Z' && chars[i] >= 'A')
+                {
+                    chars[i] = (char)((chars[i] + n));
+                    if (chars[i] > 'Z') chars[i] = (char)(chars[i] - 26);
+                }
+            }
+            // a 는 97,  소문자는 26개  A 는 65 ,
+            answer = new string(chars);
+            Console.WriteLine(answer);
+            Console.ReadLine();
+            return answer;
+        }
+    }
+}
