59bd7c0213aa920c3c58b853586404a780eeb33a
Zif1519 <141081153+Zif1519@users.noreply.github.com>
2023-09-19 08:57:45 +0900

    Date230918
    Date230918

---
+++ Date230918/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Date230918
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            Console.WriteLine(~a);
+            Console.ReadLine();
+        }
+    }
+}
